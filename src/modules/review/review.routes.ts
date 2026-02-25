7f33c608d593f7d38d233fbe1bc774ece79958be
Arsgn <blackbarber1323@gmail.com>
2026-02-25 13:29:24 +0600

    private_notification

--- src/modules/review/review.routes.ts
+++ src/modules/review/review.routes.ts
@@ -1,11 +1,11 @@
-import { Router } from "express";
-import reviewControllers from "./review.controllers";
+// import { Router } from "express";
+// import reviewControllers from "./review.controllers";
 
-const reviewRoutes = Router();
+// const reviewRoutes = Router();
 
-reviewRoutes.get("/reviews", reviewControllers.getReview);
-reviewRoutes.post("/reviews", reviewControllers.postReview);
-reviewRoutes.put("/reviews/:id", reviewControllers.putReview);
-reviewRoutes.delete("/reviews/:id", reviewControllers.deleteReview);
+// reviewRoutes.get("/reviews", reviewControllers.getReview);
+// reviewRoutes.post("/reviews", reviewControllers.postReview);
+// reviewRoutes.put("/reviews/:id", reviewControllers.putReview);
+// reviewRoutes.delete("/reviews/:id", reviewControllers.deleteReview);
 
-export default reviewRoutes;
+// export default reviewRoutes;
